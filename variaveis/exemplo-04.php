2bc85ad68d35fc65d0b2a7623e8a258776eca10e
alexandre <alexandrerochaf@hotmail.com>
2021-02-06 16:56:13 -0300

    noVOS ARQUIVOS E VARIAVEIS ESTUDADOS

---
+++ variaveis/exemplo-04.php
@@ -0,0 +1,17 @@
+<?php
+
+$nome =(int) $_GET["a"];//colocando parenteses na frente voce define o tipo de variavel por exemplo int de numeor inteiro 
+
+//var_dump($nome);
+$ip = $_SERVER["REMOTE_ADDR"];
+echo$ip;
+
+
+
+
+
+
+
+
+
+?>
